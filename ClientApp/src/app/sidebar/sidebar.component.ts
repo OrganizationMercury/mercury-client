70bebf3361d355abc6a7f3f30ff31a61ba07d811
Maxaytt <maxaytt@gmail.com>
2024-03-07 22:31:16 +0100

    feat(components): add openMenu method to sidebar

--- ClientApp/src/app/sidebar/sidebar.component.ts
+++ ClientApp/src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { ViewContainerRef, ViewChild, Component } from '@angular/core';
+import { RoundButtonComponent } from '../round-button/round-button.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { DropdownMenuComponent } from '../dropdown-menu/dropdown-menu.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RoundButtonComponent, SearchBarComponent, DropdownMenuComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  @ViewChild('menuContainer', { read: ViewContainerRef }) menuContainer!: ViewContainerRef;
 
+  openMenu = (event: MouseEvent) => {
+    if(this.menuContainer.length === 0){
+      let menu = this.menuContainer.createComponent(DropdownMenuComponent);
+      menu.instance.menuContainer = this.menuContainer;
+      event.stopPropagation();
+    }
+  }
 }
