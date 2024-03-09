9da0189e0283525f3147e53baa1f6b341afadddd
Maxaytt <maxaytt@gmail.com>
2024-03-09 16:06:03 +0100

    feat(components): add object array for images and text

--- ClientApp/src/app/dropdown-menu/dropdown-menu.component.ts
+++ ClientApp/src/app/dropdown-menu/dropdown-menu.component.ts
@@ -1,21 +1,23 @@
-import { AfterViewInit, Component, ElementRef, HostListener, Input, Renderer2, ViewContainerRef, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, Input, Renderer2, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown-menu',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './dropdown-menu.component.html',
   styleUrl: './dropdown-menu.component.css'
 })
 export class DropdownMenuComponent {
-  constructor(private el: ElementRef, private renderer: Renderer2) { }
+  @Input() mainMenu! : ViewContainerRef;
+  @Input() menuItems! : {image:string, text:string}[];
 
-  @Input() menuContainer! : ViewContainerRef;
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   deleteComponent() {
     this.renderer.addClass(this.el.nativeElement, 'fade-out');
     setTimeout(() => {
-      this.menuContainer.clear();
+      this.mainMenu.clear();
       }, 500); 
   }
 
