5e55144ddcaa552b0c4fc86002fd4a1163ab4331
Maxaytt <maxaytt@gmail.com>
2024-03-09 20:39:39 +0100

    feat(components): get src and click function from service

--- ClientApp/src/app/modules/round-button/round-button.component.ts
+++ ClientApp/src/app/modules/round-button/round-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { ButtonService } from '../../services/ButtonService';
 
 @Component({
   selector: 'app-round-button',
@@ -9,12 +10,14 @@ import { Component, Input, OnInit, HostListener } from '@angular/core';
 })
 export class RoundButtonComponent implements OnInit{
   buttonIcon!: string;
-  
+
+  constructor(private buttonService: ButtonService) {}
+
+
   ngOnInit(): void {
-      
+    this.buttonService.src$.subscribe(src => this.buttonIcon = src);
+    this.buttonService.click$.subscribe(click => this.onClick = click);
   }
 
-  @HostListener('click', ['$event']) onClick() {
-
-  }
+  @HostListener('click', ['$event']) onClick!: (event: MouseEvent) => void;
 }
