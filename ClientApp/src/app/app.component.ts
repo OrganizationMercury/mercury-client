f96a228fc16d202223ef7eecf785e68655b29a42
Maxaytt <maxaytt@gmail.com>
2024-03-15 19:46:49 +0100

    feat(components): add ng-switch logic for sidebar

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,36 +1,20 @@
-import { AfterContentChecked, AfterContentInit, ChangeDetectorRef, Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
-import { SidebarComponent } from './modules/sidebar/sidebar.component';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { DropdownMenuComponent } from './modules/dropdown-menu/dropdown-menu.component';
 import { ChatComponent } from './modules/chat/chat.component';
 import { CommonModule } from '@angular/common';
-import { ButtonService } from './services/ButtonService';
+import { SidebarModule } from './modules/sidebar/sidebar.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent, ChatComponent, CommonModule],
+  imports: [SidebarModule, ChatComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements AfterContentChecked{
-  @ViewChild('selectedSidebar', { read: ViewContainerRef }) sidebarContainer!: ViewContainerRef;
-  @ViewChild(SidebarComponent) sidebar!: SidebarComponent;
-
-  constructor(private buttonService: ButtonService, private detector: ChangeDetectorRef) {}
-
-  ngAfterContentChecked(): void {
-    //Сомнительно, надо погуглить еще. не очень использовать и детект и ngAfterContent...
-    this.buttonService.setSrc('../../../assets/menu.svg');
-    this.buttonService.setClick(this.openMenu);
-    this.detector.detectChanges();
-  }
-
-  openMenu = (event: MouseEvent) => {
-    if(this.sidebar.mainMenu.length === 0){
-      let menu = this.sidebar.mainMenu.createComponent(DropdownMenuComponent);
-      menu.instance.mainMenu = this.sidebar.mainMenu;
-      menu.instance.menuItems = this.sidebar.menuItems;
-      event.stopPropagation();
-    }
+export class AppComponent {
+  sidebarMode = 'main';
+  
+  handleModeEvent(event: string) {
+    this.sidebarMode = event;
   }
 }
