f4d271112a653a3aa58d9413707dd9c495abadd6
Maxaytt <maxaytt@gmail.com>
2024-04-19 16:06:41 +0200

    feat(components): get user data and avatar url from api

--- ClientApp/src/app/modules/sidebar/settings-sidebar/settings-sidebar.component.ts
+++ ClientApp/src/app/modules/sidebar/settings-sidebar/settings-sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { UserDto } from '../../../dto/user.dto';
 
 
 @Component({
@@ -8,8 +10,22 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class SettingsSidebarComponent {
   @Output() sidebarModeEvent = new EventEmitter<string>();
+
+  userData? : UserDto;
+  userAvatarUrl?: string; 
   headerButtonIcon = 'assets/arrow-left.svg';
   isFormOpen = false;
+
+  constructor(userService: UserService) {
+    userService.getUserAvatar().subscribe(response => {
+      console.log(response);
+      this.userAvatarUrl = response;
+    });
+    userService.getUserById().subscribe(response => {
+      console.log(response);
+      this.userData = response as UserDto;
+    })
+   }
 
   toMainSidebar = () => {
     this.sidebarModeEvent.emit('main');
