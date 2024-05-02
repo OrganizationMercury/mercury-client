451fc4eb1782be080f2cbd31f8b390dc28e7851f
Maxaytt <maxaytt@gmail.com>
2024-05-02 16:40:28 +0200

    feat(components): handle form and unlink interest

--- ClientApp/src/app/modules/sidebar/settings-sidebar/settings-sidebar.component.ts
+++ ClientApp/src/app/modules/sidebar/settings-sidebar/settings-sidebar.component.ts
@@ -1,6 +1,7 @@
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, Output, EventEmitter } from '@angular/core';
 import { UserService } from '../../../services/user.service';
-import { UserDto } from '../../../dto/user.dto';
+import { UserDto, InterestDto } from '../../../dto/user.dto';
 
 
 @Component({
@@ -13,21 +14,55 @@ export class SettingsSidebarComponent {
 
   userData? : UserDto;
   userAvatarUrl?: string; 
+  userInterests?: InterestDto[];
   headerButtonIcon = 'assets/arrow-left.svg';
   isFormOpen = false;
 
-  constructor(userService: UserService) {
+  constructor(private userService: UserService) {
     userService.getUserAvatar().subscribe(response => {
-      console.log(response);
       this.userAvatarUrl = response;
     });
     userService.getUserById().subscribe(response => {
-      console.log(response);
       this.userData = response as UserDto;
-    })
-   }
+    });
+    userService.getUserInterests().subscribe(response => {
+      this.userInterests = response as InterestDto[];
+    });
+  }
+
+  applyForm = new FormGroup({
+    Name: new FormControl(''),
+  });
 
   toMainSidebar = () => {
     this.sidebarModeEvent.emit('main');
+  }
+
+  onFormSubmit = () => {
+    const { Name } = this.applyForm.value;
+    const interest: InterestDto = { name: Name! };
+    this.userService.linkUserInterest(interest).subscribe(
+      _ => {
+        this.userInterests?.push(interest);
+        this.applyForm.reset();
+      },
+      error => {
+        console.error('Error:', error);
+    });
+  }
+
+  unlinkInterest = (interestName: string) => {
+    const interest: InterestDto = { name: interestName };
+    this.userService.unlinkUserInterest(interest).subscribe(
+      response => {
+        console.log(response)
+        const indexToRemove = this.userInterests!.findIndex(interestItem => interestItem.name === interest.name);
+        if (indexToRemove !== -1) {
+          this.userInterests!.splice(indexToRemove, 1);
+        }
+      },
+      error => {
+        console.error('Error:', error);
+    });;
   }
 }
