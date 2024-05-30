8eeaf6583bf372a2085c97206902684158bb58a6
Maxaytt <maxaytt@gmail.com>
2024-05-30 17:24:25 +0200

    feat(services): add token service

---
+++ ClientApp/src/app/services/token.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  set token(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  get token() {
+    return localStorage.getItem('token') as string;
+  }
+
+  isTokenValid() {
+    const token = this.token;
+    if (!token) {
+      return false;
+    }
+
+    const jwtHelper = new JwtHelperService();
+
+    const isTokenExpired = jwtHelper.isTokenExpired(token);
+    if (isTokenExpired) {
+      localStorage.clear();
+      return false;
+    }
+    return true;
+  }
+
+  isTokenNotValid() {
+    return !this.isTokenValid();
+  }
+}
