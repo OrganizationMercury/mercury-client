9fafb883c71ba18bc5cbd646afbb50cb0490ef59
Maxaytt <maxaytt@gmail.com>
2024-06-26 13:57:51 +0200

    feat(services): handle login

--- ClientApp/src/app/services/auth.service.ts
+++ ClientApp/src/app/services/auth.service.ts
@@ -1,19 +1,33 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { LoginDto, RegisterDto } from "../dto/auth.dto";
-import { Observable } from "rxjs";
+import { Observable, catchError, tap } from "rxjs";
+import { Router } from "@angular/router";
+import { TokenService } from './token.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    constructor(private http: HttpClient) { }
+    constructor(private router: Router, private http: HttpClient, private tokenService: TokenService) { }
 
     login(dto: LoginDto) : Observable<string> {
-        return this.http.post('http://localhost:8080/Account/Login', dto, {responseType: 'text'}) as Observable<string>;
+        return this.http.post('http://localhost:8080/Account/Login', dto, {responseType: 'text'})
+            .pipe(
+                tap((response: string) => {
+                    console.log(`token received: ${response}`);
+                    this.tokenService.token = response;
+                    this.router.navigateByUrl('home');
+                }),
+                catchError((error: any) => {
+                    console.error('Error occurred:', error);
+                    throw error;
+                })
+            );
     }
 
     register(dto: RegisterDto) {
+        console.log('register dto',dto);
         return this.http.post('http://localhost:8080/Account/Register', dto);
     }
 }
