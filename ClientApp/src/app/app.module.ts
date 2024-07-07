d02ae5d0663ab7c5c46aaee44d8582c056310eb1
Maxaytt <maxaytt@gmail.com>
2024-07-07 19:42:37 +0200

    fix: add root component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
 import { CommonModule } from "@angular/common";
 import { AppRoutingModule } from "./app-routing.module";
 import { TokenService } from "./services/token.service";
@@ -11,6 +10,7 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { LoginComponent } from "./Pages/login/login.component";
 import { RegisterComponent } from "./Pages/register/register.component";
 import { AuthService } from "./services/auth.service";
+import { AppComponent } from "./app.component";
 
 @NgModule({
     declarations: [
