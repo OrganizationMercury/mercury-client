1c40a48de9523e5fe3426cee2a361650e51f0f4f
Maxaytt <maxaytt@gmail.com>
2024-12-10 18:04:28 +0100

    feat: add app component styles

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
-  styles: ''
+  styles: `
+  :host {
+    width: 100%;
+    height: 100%;
+  }
+  `
 })
 export class AppComponent {
 
